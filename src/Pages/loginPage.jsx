4807f1ae59c665a8cba8d6a8bb3e3f57e75b972b
HariniWeerarathna <hariniiweerarathna@gmail.com>
2026-06-24 21:05:20 +0530

    Enhance login page

--- src/Pages/loginPage.jsx
+++ src/Pages/loginPage.jsx
@@ -1,14 +1,48 @@
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-hot-toast";
-
+import { useState } from "react";
+import api from "../lib/api";
 
 export default function LoginPage() {
 
-    const 
+    const [email , setEmail] = useState("")
+    const [password , setPassword] = useState("")
 
+    function handleLogin(){
+        /*toast.success(email)
+        toast.success(password) - show your email amd password as tick (as toast)*/
+    
+       /* axios.post("http://localhost:3000/users/login", 
+            {
+                email:email,
+                password:password
+            }
+        )*/
 
+        api.post("/users/login",
+            {
+                email:email,
+                password:password
+            }   
+        ).then(
+            (res)=>{
+                toast.success("Login successful.")
+                console.log(res.data.token)
+                console.log(res.data.isAdmin)
 
+                //browser store
+                localStorage.setItem("token", res.data.token)
+                const token = localStorage.getItem("token")
+
+            }
+        ).catch(
+            (err)=>{
+                console.log(err)
+                toast.error("Login failed.")
+            }
+        )
+    }
 
     return(
         <div className="w-full h-full bg-[url('/bg.jpg')] bg-cover bg-center flex justify-center items-center">
@@ -18,36 +52,38 @@ export default function LoginPage() {
                 <img src="logo.webp" className="w-[150px] h-[70px] object-cover bg-accent/40 rounded-lg"/>
                 <h1 className="text-3xl font-bold text-secondary mt-5">Login</h1>
 
+
                 <label className="w-full mt-5 text-lg text-secondary font-semibold">Email</label>
                 <input
                 onChange={
                     (e)=>{      
-                        toast.success(e.target.value)   
+                        //toast.success(e.target.value) - show updated email value in toast
+                        setEmail(e.target.value)  
                     }
                 }
                  type="email" className="w-full h-12 rounded-lg bg-secondary/20 border-2 border-accent focus:border-accent outline-none px-2 text-secondary" placeholder="user@gmail.com"/>
 
 
+
                 <label className="w-full mt-5 text-lg text-secondary font-semibold">Password</label>
                 <input
                 onChange={
-                    ()=>{
-
-                        toast.success("Password is changed")
+                    (e)=>{
+                        //toast.success(e.target.value)
+                        setPassword(e.target.value)
                     }
                 }
                 type="password" className="w-full h-12 rounded-lg bg-secondary/20 border-2 border-accent focus:border-accent outline-none px-2 text-secondary" placeholder="••••••••"/>
                 <p className="w-full  text-right">Forget Password? reset <Link to="/reset-password" className="text-accent font-bold">here</Link></p>
                 
-                <button  className="w-full h-12 bg-accent rounded-lg text-white font-bold mt-5 ">Login</button>
+
+                
+                <button onClick={handleLogin}  className="w-full h-12 bg-accent rounded-lg text-white font-bold mt-5 ">Login</button>
                 <p className="w-full  text-right">Do not have an account? register <Link to="/register" className="text-accent font-bold">here</Link></p>
 
                 <button  className="w-full h-12 bg-secondary/20 rounded-lg text-secondary font-bold mt-5 border-2 border-secondary hover:bg-secondary hover:text-white transition-colors flex items-center justify-center gap-2"><FcGoogle /> Login with Google</button>
 
-
-            </div>
-            
+            </div>     
         </div>
     )
-
 }
