5c1da57f074253f7055c4f8283ef1da39fd109cb
HariniWeerarathna <hariniiweerarathna@gmail.com>
2026-08-03 15:46:29 +0530

    Implement order pages

--- src/lib/price-format.js
+++ src/lib/price-format.js
@@ -9,10 +9,8 @@ export default function  getFormattedPrice(price){
     
     if(isNaN(priceInNumber)){
         return "N/A"
-    }else{
-        
-        return "LKR "+priceInNumber.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })    
-        
+    }else{ 
+        return "LKR "+priceInNumber.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })       
     }
 
 }
