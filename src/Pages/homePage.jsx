822c7693c7252594c325cabadbe2f3663a626a0f
HariniWeerarathna <hariniiweerarathna@gmail.com>
2026-09-15 13:51:14 +0530

    Update

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -6,12 +6,9 @@ import CartPage from "./cartPage";
 import CheckoutPage from "./checkoutPage";
 import MyOrdersPage from "./myOrdersPage";
 import SettingsPage from "./settingsPage";
-<<<<<<< Updated upstream
-=======
 import LandingPage from "./landingPage";
 import AboutPage from "./aboutPage";
 import ReviewsPage from "./reviewsPage";
->>>>>>> Stashed changes
 
 
 export default function HomePage() {
@@ -19,14 +16,9 @@ export default function HomePage() {
         <div className="min-h-full w-full bg-primary pb-[80px] lg:pb-0">
         <Header />
             <Routes>
-<<<<<<< Updated upstream
-                <Route path="/" element={<h1>Home Page</h1>} />
-                <Route path="/about" element={<h1>About Page</h1>} />
-=======
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/reviews" element={<ReviewsPage />} />
->>>>>>> Stashed changes
                 <Route path="/products" element={<ProductsPage />} />
                 <Route path="/overview/:productId" element={<ProductOverview/>} />
                 <Route path="/cart" element={<CartPage/>} />
